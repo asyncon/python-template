941af75ef5387125ea4ad2bbd76a203527a9530f
asyncon <53700266+asyncon@users.noreply.github.com>
2022-06-11 21:45:44 +1000

    Support implicit namespace packaging

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,13 +1,36 @@
 #!/usr/bin/env bash
 
 (
-    if git rev-parse --show-toplevel;then
+    if git rev-parse HEAD;then
         true
+    elif git rev-parse --show-toplevel;then
+        git branch -m main
     else
         git init -b main &&
         git remote add origin git@github.com:{{ cookiecutter.github_username }}/{{ cookiecutter.repo_name }}
     fi
 ) &&
+{%- if '.' in cookiecutter.project_slug %}
+{%- set realpath = cookiecutter.project_slug.replace('.','/') %}
+mkdir -p src/{{realpath.rsplit('/',1)[0]}} &&
+mv src/{{cookiecutter.project_slug}} src/{{realpath}} &&
+(
+    (
+    echo &&
+{%- if realpath.count('/') > 1 %}
+    echo "# Implicit namespace packaging cannot have *.py in parent's parent folders" &&
+{%- endif %}
+{%- for p in range(realpath.count('/')) %}
+{%- if loop.last %}
+    echo "# Implicit namespace packaging cannot have __init__.py in immediate parent folder" &&
+    echo "/src/{{realpath.rsplit('/',loop.length - p)[0]}}/__init__.py"
+{%- else %}
+    echo "/src/{{realpath.rsplit('/',loop.length - p)[0]}}/*.py" &&
+{%- endif %}
+{%- endfor %}
+    ) >> .gitignore
+) &&
+{%- endif %}
 export PIPENV_VENV_IN_PROJECT=1 &&
 pipenv update --dev --python={{ cookiecutter.requires_python }} &&
 pipenv run pre-commit autoupdate &&
--- {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
+++ {{cookiecutter.repo_name}}/src/{{cookiecutter.project_slug}}/__init__.py
@@ -1,7 +1,11 @@
 from importlib.metadata import PackageNotFoundError, version
 
 try:
+{%- if cookiecutter.project_slug.replace('.','-').replace('_','-') == cookiecutter.repo_name.replace('.','-').replace('_','-') %}
     __version__ = version(__name__)
+{%- else %}
+    __version__ = version("{{cookiecutter.repo_name}}")
+{%- endif %}
 except PackageNotFoundError:  # pragma: no cover
     # package is not installed
     __version__ = "0.0.0.dev0"
